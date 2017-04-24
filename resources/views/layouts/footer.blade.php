c37ccdf0ddabc8722ed4e92c648d5025b9b4545f
kawax <kawaxbiz@gmail.com>
2017-04-24 20:08:35 +0900

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="container text-center">
         <p class="text-muted">tootlog {{ config('tootlog.version') }}</p>
-        <p><a href="https://enty.jp/kawax" target="_blank">日本語での情報(Japanese Release Note)</a></p>
+        <p><a href="https://enty.jp/kawax" target="_blank">日本語での情報(Japanese Release Notes)</a></p>
     </div>
 </footer>
