8acf68cef69c80fb60083751f28731669a5d61a9
kawax <kawaxbiz@gmail.com>
2017-05-01 23:03:16 +0900

    sitemaps

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -16,16 +16,16 @@ class SitemapController extends Controller
         $sitemap->setCache('tootlog.sitemaps', 60);
 
         if (!$sitemap->isCached()) {
-            $sitemap->add(url('/'), Chronos::now(), '0.5', 'daily');
+            $sitemap->add(url('/'), Chronos::now(), '0.2', 'weekly');
 
             foreach (User::latest()->cursor() as $user) {
-                $sitemap->add(route('open.user', ['user' => $user]), $user->updated_at, '1.0', 'daily');
+                $sitemap->add(route('open.user', ['user' => $user]), $user->updated_at, '1.0', 'hourly');
             }
 
             foreach (Account::where('locked', false)->latest()->cursor() as $account) {
                 $sitemap->add(route('open.account.index',
                     ['user' => $account->user, 'username' => $account->username, 'domain' => $account->domain]),
-                    $account->updated_at, '1.0', 'daily');
+                    $account->updated_at, '0.5', 'daily');
             }
         }
 
