168fa897e91830bdf68ac146ae107f28dc1220f7
kawax <kawaxbiz@gmail.com>
2017-05-11 21:15:05 +0900

    nofollow

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,8 +51,8 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
+                        <li><a href="{{ route('login') }}" ref="nofollow">Login</a></li>
+                        <li><a href="{{ route('register') }}" ref="nofollow">Register</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
