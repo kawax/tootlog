da2e328c2f36f527adb8c0ebee8a4644b820d95a
kawax <kawaxbiz@gmail.com>
2017-09-10 11:41:31 +0900

    tp

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,9 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies = [
-        'proxies' => '*',
-    ];
+    protected $proxies;
 
     /**
      * The current proxy header mappings.
@@ -22,11 +20,10 @@ class TrustProxies extends Middleware
      * @var array
      */
     protected $headers = [
-        Request::HEADER_FORWARDED         => null,
-        Request::HEADER_X_FORWARDED_FOR   => 'X_FORWARDED_FOR',
-        Request::HEADER_X_FORWARDED_HOST  => null,
-        Request::HEADER_X_FORWARDED_PORT  => 'X_FORWARDED_PORT',
+        Request::HEADER_FORWARDED => 'FORWARDED',
+        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
         Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
-//        Request::HEADER_X_FORWARDED_AWS_ELB => null,
     ];
 }
