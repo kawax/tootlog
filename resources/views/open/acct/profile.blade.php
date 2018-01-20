ae9a4dec025fe6e039cd0a05ddcb91315b5a02af
kawax <kawaxbiz@gmail.com>
2018-01-20 16:20:02 +0900

    profile

--- resources/views/open/acct/profile.blade.php
+++ resources/views/open/acct/profile.blade.php
@@ -1,20 +1,18 @@
 <div class="card mb-3">
     <div class="card-header bg-white">
-        <h3 class="card-title">Profile</h3>
+        Profile
     </div>
     <div class="card-body">
 
         <div class="media">
-            <div class="media-left">
-                <a href="{{ $acct->url }}" target="_blank" rel="nofollow noopener">
-                    <img class="media-object rounded-circle toot-icon"
-                         src="{{ $acct->avatar }}"
-                         alt="{{ $acct->name }}"
-                         title="{{ $acct->name }}">
-                </a>
-            </div>
-            <div class="media-body">
-                <h3 class="media-heading">{!! LaravelEmojiOne::toImage($acct->name) !!}</h3>
+            <a href="{{ $acct->url }}" target="_blank" rel="nofollow noopener">
+                <img class="rounded-circle toot-icon"
+                     src="{{ $acct->avatar }}"
+                     alt="{{ $acct->name }}"
+                     title="{{ $acct->name }}">
+            </a>
+            <div class="media-body ml-3">
+                <h3>{!! LaravelEmojiOne::toImage($acct->name) !!}</h3>
                 <p>
                     <span class="badge badge-pill badge-secondary">{!! $acct->statuses_count !!} posts</span>
                     <span class="badge badge-pill badge-secondary">{!! $acct->following_count !!} follows</span>
