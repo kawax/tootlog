4c8aa451c76c5ce8b9a08dba33857752a0af0421
kawax <kawaxbiz@gmail.com>
2018-03-07 23:50:48 +0900

    v-pre

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
                 @else
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">
+                           aria-haspopup="true" aria-expanded="false" v-pre>
 
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
