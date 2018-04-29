d58b6b22946c416610eaf7f064f06ba9e3babd1e
kawax <kawaxbiz@gmail.com>
2018-04-29 17:56:32 +0900

    list

--- resources/views/open/account_list.blade.php
+++ resources/views/open/account_list.blade.php
@@ -12,13 +12,13 @@
                         <i class="fa fa-ban text-danger" aria-hidden="true"></i>
                     @else
                         <img src="{{ $account->favicon }}" width="{{ config('tootlog.favicon_size') }}"
-                             class="rounded-circle" alt="favicon">
+                             class="rounded-circle mr-1" alt="favicon">
                     @endif
 
                     {{ $account->acct }}
 
                     @if($account->locked)
-                        <i class="fa fa-lock" aria-hidden="true"></i>
+                        <i class="fa fa-lock ml-1" aria-hidden="true"></i>
                     @endif
 
                     <span class="badge badge-pill badge-secondary ml-auto">{{ $account->statuses_count }}</span>
