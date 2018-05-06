731bbfcd9301c0a4f41940714e0c014c99b0cfcf
kawax <kawaxbiz@gmail.com>
2018-05-06 14:46:42 +0900

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,8 +7,9 @@
             <span class="badge badge-pill badge-secondary">{{ $footer_statuses }} statuses</span>
         </div>
         <div>
-            <a href="https://github.com/kawax/tootlog" target="_blank" rel="noopener">GitHub</a>
-            <a href="{{ url('/usage') }}">使い方</a>
+            <a href="https://github.com/kawax/tootlog" target="_blank" rel="noopener">GitHub</a> |
+            <a href="{{ url('/usage') }}">使い方</a> |
+            <a href="https://www.pixiv.net/fanbox/creator/762638" target="_blank" rel="noopener">pixivFANBOX</a>
         </div>
     </div>
 </footer>
