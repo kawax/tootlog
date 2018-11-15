7be4dddc14697fc223d7493ba37ad04238f6305a
kawax <kawaxbiz@gmail.com>
2018-11-15 14:25:34 +0900

    tele1.0

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -18,8 +18,10 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         // Telescope::night();
 
+        $this->hideSensitiveRequestDetails();
+
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment() == 'local') {
+            if ($this->app->isLocal()) {
                 return true;
             }
 
@@ -28,6 +30,26 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
                    $entry->isScheduledTask() ||
                    $entry->hasMonitoredTag();
         });
+    }
+
+    /**
+     * Prevent sensitive request details from being logged by Telescope.
+     *
+     * @return void
+     */
+    protected function hideSensitiveRequestDetails()
+    {
+        if ($this->app->isLocal()) {
+            return;
+        }
+
+        Telescope::hideRequestParameters(['_token']);
+
+        Telescope::hideRequestHeaders([
+            'cookie',
+            'x-csrf-token',
+            'x-xsrf-token',
+        ]);
     }
 
     /**
