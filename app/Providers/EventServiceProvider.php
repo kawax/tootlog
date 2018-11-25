71cb0a82845efba6a51a3ba5e907b346f15b8822
kawax <kawaxbiz@gmail.com>
2018-11-25 21:32:40 +0900

    verify

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -19,9 +19,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\Event' => [
             'App\Listeners\EventListener',
         ],
-        Registered::class  => [
-            SendEmailVerificationNotification::class,
-        ],
+        //        Registered::class  => [
+        //            SendEmailVerificationNotification::class,
+        //        ],
     ];
 
     /**
