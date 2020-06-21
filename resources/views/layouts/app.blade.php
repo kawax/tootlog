35e4d2552c781a9dc5f105564d242f28988bb3fe
kawax <kawaxbiz@gmail.com>
2020-06-21 15:09:45 +0900

    ads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,8 @@
 
     @include('layouts.google')
 
+    @include('layouts.adsense')
+
     <meta name="google-site-verification" content="AjfwegofAkFuyDlrRBsMo8k4y6rpfEri5Qjd7uz3PbQ">
 </head>
 <body class="theme-{{ auth()->user()->theme ?? 'thin' }}">
