d70e7b1ea226478d4457fd49c55e0c31970da164
kawax <kawaxbiz@gmail.com>
2024-05-10 15:02:20 +0900

    Update MediaSwitch.vue

--- resources/js/components/MediaSwitch.vue
+++ resources/js/components/MediaSwitch.vue
@@ -6,11 +6,9 @@ const emit = defineEmits(['mediaChanged'])
 const active_media = ref('normal');
 
 const medias = {
-    normal:
-        '<i class="fa fa-file-image-o" aria-hidden="true"></i> Media Default',
+    normal: '<i class="fa fa-file-image-o" aria-hidden="true"></i> Media Default',
     only: '<i class="fa fa-picture-o" aria-hidden="true"></i> Only',
-    except:
-        '<i class="fa fa-commenting-o" aria-hidden="true"></i> Except',
+    except: '<i class="fa fa-commenting-o" aria-hidden="true"></i> Except',
 };
 
 function mediaChanged(media) {
