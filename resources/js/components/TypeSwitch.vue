c09d03cc943f2fd78b3bbccb21820dabc77f3eb7
kawax <kawaxbiz@gmail.com>
2024-05-10 15:02:22 +0900

    Update TypeSwitch.vue

--- resources/js/components/TypeSwitch.vue
+++ resources/js/components/TypeSwitch.vue
@@ -7,10 +7,8 @@ const active_type = ref('public:local');
 
 const types = {
     user: '<i class="fa fa-home" aria-hidden="true"></i> User',
-    'public:local':
-        '<i class="fa fa-users" aria-hidden="true"></i> Local',
-    public:
-        '<i class="fa fa-globe" aria-hidden="true"></i> Federated',
+    'public:local': '<i class="fa fa-users" aria-hidden="true"></i> Local',
+    public: '<i class="fa fa-globe" aria-hidden="true"></i> Federated',
 };
 
 function typeChanged(type) {
