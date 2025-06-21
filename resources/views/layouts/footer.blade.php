cb68d5aa8eab320e80b6d8b71b13548ee62fd9c8
kawax <kawaxbiz@gmail.com>
2025-06-21 16:20:45 +0900

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="bg-white border-top mb-5">
     <div class="container text-center">
-        <div class="text-muted">tootlog {{ config('tootlog.version') }}</div>
+        <div class="text-muted my-3">tootlog {{ config('tootlog.version') }}</div>
 
         <livewire:footer-info lazy></livewire:footer-info>
 
-        <div>
+        <div class="my-3">
             <a href="https://github.com/kawax/tootlog" target="_blank" rel="noopener noreferrer">GitHub</a> |
             <a href="{{ route('usage.jp') }}">使い方</a> |
             <a href="{{ route('usage.en') }}">How to Use</a>
