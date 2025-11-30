72e6a7913f861e1867a0599d0d88a67bb2e4373e
kawax <kawaxbiz@gmail.com>
2025-12-01 07:33:27 +0900

    Update home.php

--- routes/home.php
+++ routes/home.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
 Route::middleware(['auth', 'verified'])->prefix('home')->group(function () {
@@ -16,4 +17,24 @@ Route::middleware(['auth', 'verified'])->prefix('home')->group(function () {
 
     Volt::route('/{username}@{domain}', 'home.acct.index')
         ->name('home.acct.index');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
+    Volt::route('settings/password', 'settings.password')->name('user-password.edit');
+    Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
+    Volt::route('settings/export', 'settings.export')->name('settings.export');
+
+    Volt::route('settings/two-factor', 'settings.two-factor')
+        ->middleware(
+            when(
+                Features::canManageTwoFactorAuthentication()
+                && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
+                ['password.confirm'],
+                [],
+            ),
+        )
+        ->name('two-factor.show');
 });
