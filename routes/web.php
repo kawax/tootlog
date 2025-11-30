f3598416c3a7cec91663c3597f3556f86c502a05
kawax <kawaxbiz@gmail.com>
2025-12-01 07:33:29 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -42,25 +42,5 @@ Route::view('usage', 'pages.usage')->name('usage.jp');
 
 Route::view('en/usage', 'pages.en.usage')->name('usage.en');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
-    Volt::route('settings/password', 'settings.password')->name('user-password.edit');
-    Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
-    Volt::route('settings/export', 'settings.export')->name('settings.export');
-
-    Volt::route('settings/two-factor', 'settings.two-factor')
-        ->middleware(
-            when(
-                Features::canManageTwoFactorAuthentication()
-                && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
-                ['password.confirm'],
-                [],
-            ),
-        )
-        ->name('two-factor.show');
-});
-
 require __DIR__.'/home.php';
 require __DIR__.'/open.php';
