39f139d3e33d41289b68b8edf6c337463805aee7
kawax <kawaxbiz@gmail.com>
2025-11-30 16:54:27 +0900

    wip

--- resources/views/livewire/home/acct/index.blade.php
+++ resources/views/livewire/home/acct/index.blade.php
@@ -31,7 +31,7 @@ new class extends Component
 <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
     <div class="grid auto-rows-min gap-4 md:grid-cols-3">
         <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20"/>
+            @include('partials.public-url')
         </div>
         <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20"/>
---
+++ resources/views/partials/public-url.blade.php
@@ -0,0 +1,7 @@
+<div class="p-6 w-full">
+    <flux:heading size="lg" level="3">{{ __('Public URL') }}</flux:heading>
+    <flux:subheading size="md" class="mb-3">{{ __('Current URL is only visible to you. Please use the following for public URL') }}</flux:subheading>
+    <a href="{{ route('open.user', auth()->user()) }}" class="text-md underline">
+        {{ route('open.user', auth()->user()) }}
+    </a>
+</div>
