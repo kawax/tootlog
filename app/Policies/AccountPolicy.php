49b3bc7b86ae2a0761155bf6128afdcb6f83e63e
kawax <kawaxbiz@gmail.com>
2025-12-02 08:47:26 +0900

    Update AccountPolicy.php

--- app/Policies/AccountPolicy.php
+++ app/Policies/AccountPolicy.php
@@ -23,6 +23,11 @@ class AccountPolicy
         return (int) $user->id === (int) $account->user_id;
     }
 
+    public function update(User $user, Account $account): bool
+    {
+        return (int) $user->id === (int) $account->user_id;
+    }
+
     public function delete(User $user, Account $account): bool
     {
         return (int) $user->id === (int) $account->user_id;
