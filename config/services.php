6de1bdff52caa78d6d640579481cedb7138a2426
kawax <kawaxbiz@gmail.com>
2025-12-02 09:37:13 +0900

    Update services.php

--- config/services.php
+++ config/services.php
@@ -19,6 +19,8 @@ return [
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
     ],
 
     'ses' => [
