0f933f8711babff322c2260a9efbe24687a1c79f
kawax <kawaxbiz@gmail.com>
2025-12-02 09:57:24 +0900

    Update horizon.php

--- config/horizon.php
+++ config/horizon.php
@@ -199,7 +199,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default'],
+            'queue' => ['tootlog'],
             'balance' => 'auto',
             'autoScalingStrategy' => 'time',
             'maxProcesses' => 1,
