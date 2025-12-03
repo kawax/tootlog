517fd5a18ecae5484264750fe306524e1343f22f
kawax <kawaxbiz@gmail.com>
2025-12-03 13:37:22 +0900

    Update meta and favicon tags in head partial
    Added format-detection meta tag to prevent automatic detection of phone numbers and emails. Changed favicon SVG to PNG for improved compatibility.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,10 +1,11 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="format-detection" content="telephone=no, email=no" />
 
 <title>{{ $title ?? config('app.name') }}</title>
 
 <link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+<link rel="icon" href="/favicon.png" type="image/png">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
