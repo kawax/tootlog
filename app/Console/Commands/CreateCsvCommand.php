046358300b0b162f9560abe8ec04ea35e03f7533
kawax <kawaxbiz@gmail.com>
2026-01-08 19:00:15 +0900

    Add CSV generation and error notification for accounts
    Introduced CreateCsvCommand to generate CSVs for all accounts and CreateDownloadCsvJob to handle CSV creation per account. Updated GetStatusJob to trigger CSV updates for active accounts. Added FailedCreateCsv notification to alert administrators via email if CSV creation fails.

---
+++ app/Console/Commands/CreateCsvCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\CreateDownloadCsvJob;
+use App\Models\Account;
+use Illuminate\Console\Command;
+
+class CreateCsvCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'toot:create-csv';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create CSV for download';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        // アクティブ・非アクティブ含め全アカウントのCSVを作成。
+        // アクティブアカウントは更新時にCSVも更新される。
+
+        Account::query()
+            ->each(function (Account $account) {
+                CreateDownloadCsvJob::dispatch($account);
+            });
+
+        return 0;
+    }
+}
---
+++ app/Jobs/CreateDownloadCsvJob.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Account;
+use App\Models\Status;
+use App\Models\User;
+use App\Notifications\FailedCreateCsv;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+use League\Csv\CannotInsertRecord;
+use League\Csv\Exception;
+use League\Csv\Writer;
+
+class CreateDownloadCsvJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    protected Writer $writer;
+
+    protected array $files = [];
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(protected Account $account) {}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $this->write($this->account);
+    }
+
+    /**
+     * @throws CannotInsertRecord
+     * @throws Exception
+     */
+    private function write(Account $account): void
+    {
+        // 後でダウンロードする用のCSVを事前に作成。
+
+        info('Create CSV for download: '.$account->acct);
+
+        try {
+            if ($account->statuses()->count() === 0) {
+                return;
+            }
+
+            $this->writer = Writer::from(new \SplTempFileObject);
+            $this->writer->setEscape('');
+
+            $this->writer->insertOne($this->header());
+
+            $account->statuses()
+                ->withTrashed()
+                ->where('reblog_id', null)
+                ->with('account')
+                ->latest()
+                ->lazy()
+                ->each(function ($status) {
+                    $this->insert($status);
+                });
+
+            $path = 'download/'.$this->account->user->name.'/'.$account->acct.'.csv';
+
+            Storage::put($path, $this->writer);
+        } catch (\Exception $e) {
+            $error = $account->acct.': '.$e->getMessage();
+
+            logger()->error($error);
+
+            // ログが多すぎる場合は失敗する可能性があるので管理者宛に通知して確認。
+            User::find(1)->notify(new FailedCreateCsv($error));
+        }
+    }
+
+    protected function header(): array
+    {
+        return [
+            'id',
+            'content',
+            'spoiler_text',
+            'created_at',
+            'uri',
+            'url',
+        ];
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function insert(Status $status): void
+    {
+        $status_line = $status->only([
+            'status_id',
+            'content',
+            'spoiler_text',
+            'created_at',
+            'uri',
+            'url',
+        ]);
+
+        try {
+            $this->writer->insertOne($status_line);
+        } catch (CannotInsertRecord $e) {
+            logger()->error($e->getMessage());
+        }
+    }
+}
